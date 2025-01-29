f62095cb2d8d44df4874ef02d4257f3e7f8b3e16
5gtp3pp3r <william.crepault@gmail.com>
2025-01-29 09:46:15 -0500

    Mods main.tsx pour aller chercher .root comme à l'époc de creat react app

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { createRoot } from 'react-dom/client'
-import { Auth0Provider } from '@auth0/auth--react'
+import ReactDOM from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 import { App } from './App';
 
-createRoot(document.getElementById('root'))
-.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
   <Auth0Provider
-    domain="dev-vy3km5o5m55x4h6u.ca.auth0.com"
-    clientId="ygKkocSObMZMAIyoBsorMRx8CVHptNae"
-    authorizationParams={{
-      redirect_uri: window.location.origin
-    }}
-  >
+      domain="dev-tolinfz7mhsgrar6.ca.auth0.com"
+      clientId="OSNyftVG9CKEig4krBBdgx23HfTknQdj"
+      authorizationParams={{redirect_uri: window.location.origin}}
+    >
     <App />
   </Auth0Provider>
 );
