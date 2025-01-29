689612c6d27bd2f3ee78dbc4cc9d0bbf30095c1c
2394947 <Jennifer.m1999@outlook.fr>
2025-01-29 09:37:17 -0500

    Modif main

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Auth0Provider } from '@auth0/auth--react'
 import { App } from './App';
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+createRoot(document.getElementById('root'))
+.render(
+  <Auth0Provider
+    domain="dev-vy3km5o5m55x4h6u.ca.auth0.com"
+    clientId="ygKkocSObMZMAIyoBsorMRx8CVHptNae"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}
+  >
     <App />
-  </StrictMode>,
-)
+  </Auth0Provider>
+);
