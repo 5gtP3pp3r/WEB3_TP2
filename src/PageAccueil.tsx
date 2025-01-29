c4cd468e75ceb6fd25526cc55d9b5da3443779fd
5gtp3pp3r <william.crepault@gmail.com>
2025-01-29 10:01:40 -0500

    mods page accueil ajout bouton connect temp

--- src/PageAccueil.tsx
+++ src/PageAccueil.tsx
@@ -1,10 +1,29 @@
+import { Button } from 'react-bootstrap';
+import { useAuth0 } from '@auth0/auth0-react';
+import { useNavigate } from 'react-router';
+
 export function PageAccueil() {
+    const navigate = useNavigate();
+    const { isAuthenticated, loginWithRedirect} = useAuth0();
+
     return (
         <div>
             <h3>Page d'accueil</h3>
-            <p>BLABLABLA  Allez voir les jeux bande de con!</p>
+            
+            <div>
+            {isAuthenticated ? (
+                <>
+                    <p>BLABLABLA  Allez voir les jeux bande de con!</p>
+                    <Button variant='light'  onClick={() => navigate("/projets/demineur")} className="btn btn-primary">Voir les Jeux</Button>
+                </> 
+            ) : (
+                <>
+                    <p>BLABLABLA  Connectez vous bande de con!</p>
+                    <Button variant='light'  onClick={() => loginWithRedirect()}>Se connecter</Button> 
+                </>
+            )}
             <div className="d-flex justify-content-center m-5">
-                <a href="/projets/demineur" className="btn btn-primary">Voir les Jeux</a>
+            </div>    
             </div>
         </div>
         
