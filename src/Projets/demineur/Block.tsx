989e5493bd9b77c03846fee4101b5a1207cc4532
5gtp3pp3r <william.crepault@gmail.com>
2025-01-29 11:45:51 -0500

    Ajout interface IBlock et commentaires pour suivre

--- src/Projets/demineur/Block.tsx
+++ src/Projets/demineur/Block.tsx
@@ -1,8 +1,10 @@
 
 export interface IBlock {
-    x: string;
+    x: string;              // coordonnées
     y: string;
-    cache: boolean;
-    drapeau: boolean;
-    mine: boolean;
+    id: number;             // id pour qt block à générer aléatoire
+    valeur: string;         // Si caché faux: images terre1 à terre8
+    cache: boolean;         // vrai image: herbe
+    drapeau: boolean;       // Si caché vrai et rightclick: image herbeFlag
+    mine: boolean;          // à la fin du jeu, présenter les mines: caché faux, image terre1 à terre8 ou mine vrai terreMine
 }
