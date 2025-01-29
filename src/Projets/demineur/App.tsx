59c76c8186792e1eb28a6ca515d95660eaf12362
5gtp3pp3r <william.crepault@gmail.com>
2025-01-29 14:10:25 -0500

    Ajouts commentaires là où je suis rendu avec les conditions d'états, affichage de la grille, condition de changement d'image etc...

--- src/Projets/demineur/App.tsx
+++ src/Projets/demineur/App.tsx
@@ -1,5 +1,6 @@
-//import { useState } from 'react'
-// Ajouter les imports au besoin
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 import { IBlock } from "./IBlock";
 
@@ -7,7 +8,9 @@ const largeur = 10;
 const hauteur = 10;
 
 export function App() {
-
+  // pour l'istant, crée une grille générique de 10x10 sans mines aléatoire. 
+  // ne vérifie donc pas les blocka autour pour placer la valeurs en 
+  // conséquence de la proximité d'un block avec block.mine = vrai.
   const [grille, setGrille] = useState<IBlock[]>(
     Array.from({ length: largeur * hauteur }, (_, id) => ({
       x: id % largeur,
@@ -19,16 +22,16 @@ export function App() {
       mine: false,
     }))
   );
-  
-  const handleLeftClick = (id: number) => {   // méthode temporaire pour changer herbe à terre
+  // méthode temporaire pour changer herbe à terre block.cache: true/false
+  const handleLeftClick = (id: number) => {   
     setGrille((prevGrille) =>
       prevGrille.map((block) =>
         block.id === id ? { ...block, cache: false } : block
       )
     );
   };
-
-  const handleRightClick = (id: number) => {   // méthode temporaire pour afficher le flag
+  // méthode temporaire pour afficher le flag block.drapeau: true/false
+  const handleRightClick = (id: number) => {   
     setGrille((prevGrille) =>
       prevGrille.map((block) =>
         block.id === id ? { ...block, drapeau: true } : block
@@ -36,39 +39,45 @@ export function App() {
     );
   };
 
-
   return (
-    <>
-      <h1>Test affichage grid:</h1>
-      <div className="d-flex justify-content-center">
-        <div style={{ display: "grid", gridTemplateColumns: `repeat(${largeur}, 20px)`}}>
-          {grille.map((block) => (
-            <div key={block.id} style={{ width: "20px", height: "20px" }}
+    <Container>
+      <Row>
+        <Col>
+          <h1>Test affichage grid:</h1>
+          <p>Pour l'instant, dimension grille hard codé à 10x10:<br />Test de changement d'état des blocks:<br />
+          Clique gauche, tourne en terre si herbe<br />Clique droit, plante un flag si herbe<br />
+          Clique gauche ou droit de fait rien sur terre</p>
+        </Col>
+        <Col>          
+          <div style={{ display: "grid", gridTemplateColumns: `repeat(${largeur}, 20px)`}}>
+            {grille.map((block) => (
+              <div key={block.id} style={{ width: "20px", height: "20px" }}
 
-              onClick={() => {handleLeftClick(block.id)}}
-              onMouseOver={() =>console.log("x: "+block.x+"\n"+
-                                            "y: "+block.y+"\n"+
-                                            "id: "+block.id+"\n"+
-                                            "valeur: "+block.valeur+"\n"+
-                                            "caché: "+block.cache+"\n"+
-                                            "drapeau: "+block.drapeau+"\n"+
-                                            "mine: "+block.mine)}  
-              onContextMenu={(e) => { e.preventDefault(); handleRightClick(block.id);}}>
+                onClick={() => {handleLeftClick(block.id)}}
+                onMouseOver={() =>console.log("x: "+block.x+"\n"+                           // Diag test valeur au mouseOver console.log()
+                                              "y: "+block.y+"\n"+
+                                              "id: "+block.id+"\n"+
+                                              "valeur: "+block.valeur+"\n"+
+                                              "caché: "+block.cache+"\n"+
+                                              "drapeau: "+block.drapeau+"\n"+
+                                              "mine: "+block.mine)}  
+                onContextMenu={(e) => { e.preventDefault(); handleRightClick(block.id);}}>
 
-              {block.cache ? (                                                      // Condition temporaire pour changer les états 
-                block.drapeau ? (                                                   // caché et drapeau sur clique gauche et droit.
-                  <img src="../../images/demineur/herbeFlag.png" alt="herbeFlag" />
+                {block.cache ? (                                                            // Condition temporaire pour changer les états 
+                  block.drapeau ? (                                                         // caché et drapeau sur clique gauche et droit.
+                    <img src="../../images/demineur/herbeFlag.png" alt="herbeFlag" />
+                  ) : (
+                    <img src="../../images/demineur/herbe.png" alt="herbe" />
+                  )
                 ) : (
-                  <img src="../../images/demineur/herbe.png" alt="herbe" />
-                )
-              ) : (
-                <img src="../../images/demineur/terre.png" alt="terre" />
-              )}
+                  <img src="../../images/demineur/terre.png" alt="terre" />
+                )}
 
-            </div>
-          ))}
-        </div>
-      </div>
-    </>
+              </div>
+            ))}
+          </div>          
+        </Col>
+      </Row>
+    </Container>
   );
 }
