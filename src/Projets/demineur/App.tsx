f377ce103a9d1672aeb13e26c4681ab02b6e6f96
5gtp3pp3r <william.crepault@gmail.com>
2025-01-30 11:35:35 -0500

    Mods génération Grille, à isoler dans une fonction (fichier) à la demande

--- src/Projets/demineur/App.tsx
+++ src/Projets/demineur/App.tsx
@@ -4,36 +4,62 @@ import Col from 'react-bootstrap/Col';
 import { useState } from 'react';
 import { IBlock } from "./IBlock";
 
-const largeur = 30;   // facile: 8x8 10 mines, intermédiare: 16x16 40 mines, expert: 24x24 99 mines 
-const hauteur = 30;
+import { SelectionJeu } from './SelectionJeu';
+import { INiveau } from './Niveau';
+import { niveauxTab } from './Niveau';
+import { GenerateurMinesAleatoire } from './GenerateurMinesAleatoire';
+
 
 export function App() {
+  const [ niveau, setNiveau ] = useState<INiveau>({difficulte:"facile", dimensions: 8, qtMines: 10}); 
+  const [ niveauActif, setNiveauActif ] = useState<string>("facile");
+  const [ minesTab, setMinesTab ] = useState<number[]>([0]);
+
+  const handleNiveauSelect = (niveau: string) => {
+    setNiveauActif(niveau);
+    const niveauChoisi = niveauxTab.find((diff) => diff.difficulte === niveau);
+    if (niveauChoisi) {
+      setNiveau(niveauChoisi)
+    }
+  };
+  const remplirMinesTab = () => {
+    setMinesTab(GenerateurMinesAleatoire(niveau));
+  }
+  
+  //minesTab = GenerateurAleatoire({difficulte, });
   // pour l'istant, crée une grille générique de 10x10 sans mines aléatoire. 
   // ne vérifie donc pas les block autour pour placer la valeurs en 
   // conséquence de la proximité d'un block avec block.mine = vrai.
 
-  // Créer un nouveau tableau vide, ajouter la valeur "id" avec une boucle -> check 
-  // à chaque itération calculer les valeurs de x: valeur id modulo largeur -> check 
-  // y: id divisé par largeur arrondi à la baisse car les résultats seront float la majorité du temps -> check
-  // Pour l'instant mine est générique à faux. Va faloir ajouter un random vs dimension grille. -> ToDo
-  // Pour l'instant valeur générique "terre". Va faloir vérifier les voisins et 
-  // ajuster la valeur à terre${nb block voisin.mine = vrai} "terre1, terre2..." -> ToDo
+  // Créer un nouveau tableau vide, ajouter la valeur "id" avec une boucle -> CHECK 
+  // à chaque itération calculer les valeurs de x: valeur id modulo largeur -> CHECK 
+  // y: id divisé par largeur arrondi à la baisse car les résultats seront float la majorité du temps -> CHECK
+  // Pour l'instant mine est générique à faux. Va faloir ajouter un random vs dimension grille. -> CHECK
+  // Pour l'instant valeur générique 0. Va faloir vérifier les voisins et 
+  // ajuster la valeur à terre${nb block voisin.mine = vrai} "terre1, terre2..." -> TODO
   const [grille, setGrille] = useState<IBlock[]>(() => {
     const grid: IBlock[] = [];   
-    for (let id = 0; id < largeur * hauteur; ++id) {
-      const x = id % largeur; 
-      const y = Math.floor(id / largeur); 
-      // Je crois que je vais ajouter une fonction externe qui va me retourné des id aléatoires 
-      // pour passer mine à vrai. à voir comment implanter `ca vers ici avant le grid.push
-      // Vérifications des mines des voisins ici pour modifier la "valeur".
+    const qtBlock = Math.sqrt(niveau.dimensions);
+    remplirMinesTab();
+    const valeur = 0;
+    const cache = true;             // Mettre dans un Fichier à part pour être appelé avec un bouton 
+    const drapeau = false;
+    let mine = false;    
+    for (let id = 0; id < qtBlock; ++id) {
+      const x = id % niveau.dimensions; 
+      const y = Math.floor(id / niveau.dimensions); 
+      mine = minesTab.includes(id) ? true : false;      
+      // Je crois que je vais ajouter une fonction externe qui va me retourné des id aléatoires -> CHECK
+      // pour passer mine à vrai. à voir comment implanter `ca vers ici avant le grid.push -> CHECK
+      // Vérifications des mines des voisins ici pour modifier la "valeur". -> TODO
       grid.push({
         x,
         y,
         id,
-        valeur: "terre",   // Surement changer en number pour pouvoir l'utiliser dans une variable et aller chercher l'image terre${valeur}.png
-        cache: true,
-        drapeau: false,
-        mine: false,
+        valeur,   
+        cache,
+        drapeau,
+        mine,
       });
     }    
     return grid;
@@ -64,9 +90,14 @@ export function App() {
           <p>Pour l'instant, dimension grille hard codé à 10x10:<br />Test de changements d'états des blocks:<br />
           Clique gauche, tourne en terre si herbe.<br />Clique droit, plante un flag si herbe.<br />
           Clique gauche ou droit de fait rien sur terre.<br />Ce côté va être la zone d'interface pour générer le jeux.</p>
+          <SelectionJeu
+            niveaux={niveauxTab}
+            onNiveauSelect={handleNiveauSelect}
+            niveauActif={niveauActif}
+          />
         </Col>
         <Col xs={8} className='d-flex justify-content-start'>          
-          <div style={{ display: "grid", gridTemplateColumns: `repeat(${largeur}, 20px)`}}>
+          <div style={{ display: "grid", gridTemplateColumns: `repeat(${niveau.dimensions}, 20px)`}}>
             {grille.map((block) => (
               <div key={block.id} style={{ width: "20px", height: "20px", cursor: block.cache ? 'url(../../images/demineur/curseurDemineur.png), auto' : 'auto' } }
 
@@ -84,12 +115,12 @@ export function App() {
 
                 {block.cache ? (                                                            // Conditions temporaires pour changer les états 
                   block.drapeau ? (                                                         // "cache" et "drapeau" sur clique gauche et droit.
-                    <img src="../../images/demineur/herbeFlag.png" alt="herbeFlag" />       // Revoir les conditions pour enlever le flag avec un rightClick.
+                    <img src="../../images/demineur/herbeDrapeau.png" alt="herbeDrapeau" />    // Revoir les conditions pour enlever le flag avec un rightClick.
                   ) : (                                                                     // Revoir les conditions pour ne pas pouvoir faire de leftClick si flag.
                     <img src="../../images/demineur/herbe.png" alt="herbe" />
                   )
                 ) : (
-                  <img src="../../images/demineur/terre.png" alt="terre" />
+                  <img src={`../../images/demineur/terre${block.valeur}.png`} alt="terre" />
                 )}
 
               </div>
