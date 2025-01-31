d3611ed66f48a6eb4f24387e6288b148677be8bc
5gtp3pp3r <william.crepault@gmail.com>
2025-01-30 20:33:56 -0500

    Ajuster syntaxe, ajoutboucle pour valeur block (1 à 8)

--- src/Projets/demineur/GenererGrille.tsx
+++ src/Projets/demineur/GenererGrille.tsx
@@ -1,24 +1,22 @@
 import { INiveau } from "./Niveau";
 import { IBlock } from "./IBlock";
 import { GenerateurMinesAleatoire } from "./GenerateurMinesAleatoire";
+import { ChercherNbMinesVoisins } from "./ChercherNbMinesVoisins";
 
 export function GenererGrille(niveau: INiveau): IBlock[] {
-
     const minesTab = GenerateurMinesAleatoire(niveau);
-    const grid: IBlock[] = [];   
-    const qtBlock = Math.sqrt(niveau.dimensions);
-    const valeur = 0;
-    const cache = true;             // Mettre dans un Fichier à part pour être appelé avec un bouton 
+    const grille: IBlock[] = [];   
+    const qtBlock = Math.pow(niveau.dimensions,2);
+    let valeur = 0;
+    const cache = true;             
     const drapeau = false;
     let mine = false;    
     for (let id = 0; id < qtBlock; ++id) {
         const x = id % niveau.dimensions; 
         const y = Math.floor(id / niveau.dimensions); 
-        mine = minesTab.includes(id) ? true : false;      
-        // Je crois que je vais ajouter une fonction externe qui va me retourné des id aléatoires -> CHECK
-        // pour passer mine à vrai. à voir comment implanter `ca vers ici avant le grid.push -> CHECK
-        // Vérifications des mines des voisins ici pour modifier la "valeur". -> TODO
-        grid.push({
+        mine = minesTab.includes(id) ? true : false;     
+        valeur = mine ? 9 : 0;   
+        grille.push({
             x,
             y,
             id,
@@ -28,5 +26,10 @@ export function GenererGrille(niveau: INiveau): IBlock[] {
             mine,
         });
     }    
-    return grid;
+    grille.forEach((block) => {
+        if (!block.mine) {
+            block.valeur = ChercherNbMinesVoisins(niveau, block, grille);
+        }
+    })
+    return grille;
 }
