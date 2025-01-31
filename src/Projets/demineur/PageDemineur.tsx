96615c3ccde6c6a010eea0251e6dc78170201b32
5gtp3pp3r <william.crepault@gmail.com>
2025-01-30 20:34:17 -0500

    Ajout défénition retour fonction PageDemineur

--- src/Projets/demineur/PageDemineur.tsx
+++ src/Projets/demineur/PageDemineur.tsx
@@ -1,7 +1,7 @@
 import { PageTemplate } from '../../PageTemplate';
 import { App } from './App';  
 
-export function PageDemineur() {
+export function PageDemineur(): JSX.Element {
   return (
     <PageTemplate title="Jeu Démineur" previous="/projets/yahtzee" nextPage="/projets/horloge">
       {<App />}
