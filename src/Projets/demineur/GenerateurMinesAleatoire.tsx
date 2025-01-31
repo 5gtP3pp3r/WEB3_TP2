17ef69cac206d70bd0d776edc242acfd12afe696
5gtp3pp3r <william.crepault@gmail.com>
2025-01-30 20:33:11 -0500

    Mods logique de génération d'index aléatoire pour des mines

--- src/Projets/demineur/GenerateurMinesAleatoire.tsx
+++ src/Projets/demineur/GenerateurMinesAleatoire.tsx
@@ -2,14 +2,15 @@ import { INiveau } from "./Niveau";
 
 
 export function GenerateurMinesAleatoire({dimensions, qtMines}: INiveau):number[] {
-    const qtBlocks: number = Math.sqrt(dimensions);
-    const minesTab: number[] = [];
-    for (let index = 0; index < qtBlocks; ++index) {
-        let indexMine: number = Math.floor(Math.random() * (qtMines - 0 + 1)) + 0;
-        while (!minesTab.includes(indexMine)) {
-            indexMine = Math.floor(Math.random() * (qtMines - 0 + 1)) + 0;
+    const qtBlocks: number = Math.pow(dimensions,2);
+    const minesTab: number[] = [qtBlocks];
+
+    while (minesTab.length < qtMines) {
+        const indexMine = Math.floor(Math.random() * qtBlocks);
+        if (!minesTab.includes(indexMine)) {
             minesTab.push(indexMine);
-        }        
-    }    
-    return minesTab;
+        }
+    }   
+
+    return minesTab.sort();
 }
