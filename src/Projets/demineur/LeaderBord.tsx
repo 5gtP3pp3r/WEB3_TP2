d06065ff2ea1b0762755dd33a9d037953effdce9
5gtp3pp3r <william.crepault@gmail.com>
2025-02-02 15:51:40 -0500

    Ajout d,un leader bord temporaire pour vérifier l'affichage et la distribution des largueurs des colonnes

---
+++ src/Projets/demineur/LeaderBord.tsx
@@ -0,0 +1,13 @@
+
+export function LeaderBord() {
+    return (
+        <>
+        <h1>Leader bord:</h1>
+        <ul>
+            <li>Test affichage 1</li>
+            <li>Test affichage 1</li>
+            <li>Test affichage 1</li>
+        </ul>
+        </>
+    );
+}
