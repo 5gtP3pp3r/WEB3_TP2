3f363bf9746ec943077b582f44848aa318075d2e
5gtp3pp3r <william.crepault@gmail.com>
2025-02-02 22:48:20 -0500

    Ajout logique absolute pour afficher mes images pour lesd nombres négatifs

--- src/Projets/demineur/StatsJeu.tsx
+++ src/Projets/demineur/StatsJeu.tsx
@@ -5,11 +5,14 @@ interface StatsJeuProps {
 }
 
 export function StatsJeu({ temps, nbMine }: StatsJeuProps): JSX.Element {   
-    let minute  = Math.floor(temps / 60);   
-    let secondeDizaine = temps % 60 < 0 ? 0 : Math.floor((temps % 60) / 10);
-    let secondeUnite = temps % 10;
-    const dizaine = Math.floor(nbMine / 10);
-    const unite = nbMine % 10; 
+    let minute: number  = Math.floor(temps / 60);   
+    let secondeDizaine: number = temps % 60 < 0 ? 0 : Math.floor((temps % 60) / 10);
+    let secondeUnite: number = temps % 10;
+
+    const absNbMine = Math.abs(nbMine);
+    const dizaine: number = Math.floor(absNbMine / 10);
+    const unite: number = absNbMine % 10; 
+    const prefix: string = nbMine >= 0 ? "Null" : "Negatif"; 
 
     if (temps > 599) {
         minute = 8;
@@ -22,15 +25,16 @@ export function StatsJeu({ temps, nbMine }: StatsJeuProps): JSX.Element {
             <div className="position-relative">
                 <img src="../../images/demineur/nbMinesFond.png" alt="nbMinesFond" className="img-fluid"/>
                 <div className="position-absolute top-50 start-50 translate-middle d-flex">
-                    <img src={`../../images/demineur/digit${dizaine}.png`} alt="dizaine"  />
+                    <img src={`../../images/demineur/digit${prefix}.png`} alt="prefix"/>
+                    <img src={`../../images/demineur/digit${dizaine}.png`} alt="dizaine"/>
                     <img src={`../../images/demineur/digit${unite}.png`} alt="unite"/>
                 </div>
             </div>
             <div className="position-relative">
                 <img src="../../images/demineur/timerFond.png" alt="timerFond" className="img-fluid"/>
                 <div className="position-absolute top-50 start-50 translate-middle d-flex">
-                    <img src={`../../images/demineur/digit${minute}.png`} alt="minute" />
-                    <img src="../../images/demineur/digitPts.png" alt=":" />
+                    <img src={`../../images/demineur/digit${minute}.png`} alt="minute"/>
+                    <img src="../../images/demineur/digitPts.png" alt=":"/>
                     <img src={`../../images/demineur/digit${secondeDizaine}.png`} alt="seconde dizaine"/>
                     <img src={`../../images/demineur/digit${secondeUnite}.png`} alt="seconde unite" />
                 </div>
