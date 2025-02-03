6fcbd56d2b463d356431837ef9024c1d619d93cf
5gtp3pp3r <william.crepault@gmail.com>
2025-02-02 21:16:35 -0500

    mods affichage LeaderBord ajout comms

--- src/Projets/demineur/LeaderBord.tsx
+++ src/Projets/demineur/LeaderBord.tsx
@@ -1,13 +1,14 @@
 
-export function LeaderBord() {
+export function LeaderBord(): JSX.Element {
     return (
-        <>
-        <h3>Leader bord:</h3>
-        <ul>
-            <li>Test affichage leader 1</li>
-            <li>Test affichage leader 2</li>
-            <li>Test affichage leader 3</li>
-        </ul>
-        </>
+        <div className="d-flex-justify-content-center">
+            <h3>Leader bord:</h3>
+            <p>À implémenter avec appel API</p>
+            <ul>
+                <li>Test affichage leader 1</li>
+                <li>Test affichage leader 2</li>
+                <li>Test affichage leader 3</li>
+            </ul>
+        </div>
     );
 }
