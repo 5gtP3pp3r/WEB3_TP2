904c5f3e17535570ba58fc6114e0876494417ba6
5gtp3pp3r <william.crepault@gmail.com>
2025-02-03 03:11:00 -0500

    Modifications logique des points

--- src/Projets/demineur/ResultatsJeu.tsx
+++ src/Projets/demineur/ResultatsJeu.tsx
@@ -14,18 +14,21 @@ interface ResultatJeuProps {
 // plus on prend de temps plus de points son perdus.
 // Ensuite, si le résultat est toujours positif, on soustrait le nombres de clicks, autant gauche (découvrir block) que droit (mettre un drapeau).
 // Mettre et enlever un drapeau à répétitions sur un même block réduit donc les points d'avantage.
+// On ajoute une pénalitée pour avoir touché une mine. On perd le nombres de mines non trouvées * 10 points
 // En dernier on ajoute le nombre de mines découvertes. 
 
 export function ResultatJeu({niveau, nbMinesTrouves, tempsSecondes, nbClicks, estEnJeu}: ResultatJeuProps): JSX.Element {
-    const { difficulte, pointsBase } = niveau;
+    const { difficulte, pointsBase, qtMines } = niveau;
 
     const minute: number = tempsSecondes > 59 ? Math.floor(tempsSecondes / 60) : 0;  
     const seconde: number = tempsSecondes % 60;
     const minuteString: string = minute > 0 ? " minutes, " : "";
 
-    const pointsTemps = pointsBase - tempsSecondes > 0 ? pointsBase - tempsSecondes : 0;
-    const pointsClicks = pointsTemps - nbClicks > 0 ? pointsTemps - nbClicks : 0;
-    const pontTotal = pointsClicks + nbMinesTrouves;
+    const pointsApresTemps = Math.max(pointsBase - tempsSecondes, 0);
+    const pointsApresClicks = Math.max(pointsApresTemps - nbClicks, 0);
+    const penaliteMinesManquantes = (qtMines - nbMinesTrouves) * 50;
+    const pointsApresPenalite = Math.max(pointsApresClicks - penaliteMinesManquantes, 0);
+    const pointsTotal = pointsApresPenalite + (nbMinesTrouves * 5);
 
     return estEnJeu ? (
         <div className="d-flex-justify-content-center" style={{ height:'175px'}}>    
@@ -38,7 +41,7 @@ export function ResultatJeu({niveau, nbMinesTrouves, tempsSecondes, nbClicks, es
                 <li>Niveau: {difficulte}</li>
                 <li>Mines trouvé:  {nbMinesTrouves}</li>
                 <li>Temps: {minute}{minuteString}{seconde} secondes</li>
-                <li>Points total: {pontTotal}</li>
+                <li>Points total: {pointsTotal}</li>
             </ul>        
         </div>
     );
