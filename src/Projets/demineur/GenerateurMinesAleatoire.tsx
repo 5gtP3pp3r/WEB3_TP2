8ef882f26210d6cd132cd912a56916965300bb0a
5gtp3pp3r <william.crepault@gmail.com>
2025-02-03 01:39:37 -0500

    remise des Console.log GenerateurMinesAleatoire pour tests

--- src/Projets/demineur/GenerateurMinesAleatoire.tsx
+++ src/Projets/demineur/GenerateurMinesAleatoire.tsx
@@ -3,14 +3,19 @@ import { INiveau } from "./Niveau";
 
 export function GenerateurMinesAleatoire({dimensions, qtMines}: INiveau):number[] {
     const qtBlocks: number = Math.pow(dimensions,2);
-    const minesTab: number[] = [qtBlocks];
+    const minesTab: number[] = [];
 
     while (minesTab.length < qtMines) {
-        const indexMine = Math.floor(Math.random() * qtBlocks);
+        const indexMine = Math.floor(Math.random() * (qtBlocks - 1)); //Math.floor(Math.random() * (max - min + 1)) + min;
         if (!minesTab.includes(indexMine)) {
             minesTab.push(indexMine);
         }
     }   
+    console.log("Valeurs dans le générateur de mines: ");
+    console.log("Dimensions: "+dimensions);
+    console.log("Qt mines: "+qtMines);
+    console.log("qt blocks: "+qtBlocks);
+    console.log("Tableau de mines: "+minesTab.sort());
 
-    return minesTab.sort();
+    return minesTab;
 }
