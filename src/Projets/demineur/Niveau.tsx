f6402fcd9c9af07d1da76c0f0984262e0fda0afb
5gtp3pp3r <william.crepault@gmail.com>
2025-02-02 21:16:52 -0500

    Ajout points de base aux niveaux

--- src/Projets/demineur/Niveau.tsx
+++ src/Projets/demineur/Niveau.tsx
@@ -2,23 +2,27 @@
 export interface INiveau {
     difficulte: string,
     dimensions: number,
-    qtMines: number
+    qtMines: number,
+    pointsBase: number
 }
 
 export const niveauxTab: INiveau[] = [
     {
         difficulte: "facile",
         dimensions: 8,
-        qtMines: 10
+        qtMines: 10,
+        pointsBase: 500
     },
     {
         difficulte: "intermediaire",
         dimensions: 16,
-        qtMines: 40
+        qtMines: 40,
+        pointsBase: 1000
     },
     {
         difficulte: "expert",
         dimensions: 24,
-        qtMines: 99
+        qtMines: 99,
+        pointsBase: 1500
     }
 ]
