1b8c57d77ed88c901e40392929b3a901e34db3da
5gtp3pp3r <william.crepault@gmail.com>
2025-02-04 15:33:59 -0500

    Ajout temp ChoisirNomJoueur

---
+++ src/Projets/demineur/ChoisirNomJoueur.tsx
@@ -0,0 +1,75 @@
+import { Form } from "react-bootstrap";
+import ListGroup from 'react-bootstrap/ListGroup';
+import { IJoueur } from "./IJoueur";
+
+interface ListeJoueursProps {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+import ListGroup from 'react-bootstrap/ListGroup';
+import Form from 'react-bootstrap/Form';
+import { Button } from 'react-bootstrap';
+import { INiveau } from './Niveau';
+//import { niveauxTab } from './Niveau';
+
+interface NiveauProps {
+    niveaux: INiveau[];    
+    niveauActif: string;
+    onNiveauSelect: (niveau: string) => void;
+
+}
+
+export function SelectionJeu({niveaux, niveauActif, onNiveauSelect, onLancerJeu}: NiveauProps): JSX.Element {
+    const niveau = niveaux.find(n => n.difficulte === niveauActif);
+
+    return (
+        <Form>
+        <ListGroup>
+            {niveaux.map((niveau) => (
+                <ListGroup.Item
+                    key={niveau.difficulte}
+                    active={niveau.difficulte === niveauActif}
+                    onClick={() => onNiveauSelect(niveau.difficulte)} 
+                    style={{ cursor: 'pointer', width: '130px' }}
+                    variant='light'
+                    >
+                    {niveau.difficulte}
+                </ListGroup.Item>
+            ))}
+        </ListGroup>
+            <Button
+                className='mt-3'
+                variant='secondary'
+                onClick={() =>{ if(niveau) onLancerJeu(niveau) }}    // if(niveau): enlevé soulignement "undefined possible"
+                style={{ width: '130px', cursor: 'url(../../images/demineur/curseurDemineur.png), auto'}}
+                >Jouer!
+            </Button>
+        </Form>
+    );
+}
+*/
