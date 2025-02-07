9c6593ebab779cb391b98acc69ed908320598a9a
Elie Caron <elie_caron@outlook.com>
2025-02-07 15:39:43 -0500

    Yahtzee complété

--- src/Projets/yahtzee/PageYahtzee.tsx
+++ src/Projets/yahtzee/PageYahtzee.tsx
@@ -1,11 +1,11 @@
 import { PageTemplate } from '../../PageTemplate';
-import { Game } from './Game';
+import { Yahtzee } from './Yahtzee';
 
 
 export function PageYahtzee() {
   return (
     <PageTemplate title="Yahtzee!" previous="/projets/horloge" nextPage="/projets/demineur">
-      {<Game/>}
+      {<Yahtzee/>}
     </PageTemplate>
   );
 }
